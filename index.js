bce8f1332a688ee1e19478f1ed48e2d8a4c3a809
MJU <marie.ju2012@outlook.com>
2020-04-02 00:52:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function findBy(array, findFn) {
   console.log(array)
   let found = null
   for (let i = 0; i < array.length; ++i) {
-    console.log("-> ", array[i])
+    console.log("-> ", array[i], "\n")
     if (findFn === array[i]) {
       found = array[i]
       break
